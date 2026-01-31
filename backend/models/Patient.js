1904f8da15b18bf7390e7c759de1303c9a268955
kua14108-code <kua14108@gmail.com>
2026-01-31 13:55:04 +0500

    Create Patient.js

---
+++ backend/models/Patient.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+    gender: {
+      type: String,
+      enum: ['male', 'female', 'other'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Patient', patientSchema);
