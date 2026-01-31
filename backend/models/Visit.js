afa345fcd148ce862b2a2e6167b1f7403dbf0510
kua14108-code <kua14108@gmail.com>
2026-01-31 14:22:55 +0500

    Create Visit.js

---
+++ backend/models/Visit.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const visitSchema = new mongoose.Schema(
+  {
+    patientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Patient',
+      required: true,
+    },
+    visitDate: {
+      type: Date,
+      required: true,
+    },
+    diagnosis: {
+      type: String,
+      required: true,
+    },
+    notes: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Visit', visitSchema);
