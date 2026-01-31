adfe72059830ba0fc9c7d15c3efe5e57bbba3b1b
kua14108-code <kua14108@gmail.com>
2026-01-31 14:24:59 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ connectDB();
 app.use(express.json());
 
 app.use('/api/patients', require('./routes/patients'));
+app.use('/api/visits', require('./routes/visits'));
+
 
 app.get('/', (req, res) => {
   res.json({ message: 'Clinic API is running' });
